73da07685c6d14dc2584c6ff5753b414caeae8eb
Rahul Gopinath <rahul@gopinath.org>
2017-09-08 14:32:23 +0200

    update

---
+++ tests/complex.py
@@ -0,0 +1,10 @@
+x = input()
+y = input()
+if y < 10:
+    while x > 1:
+        if x < 2:
+           print x
+        else:
+           print 0
+        x = x - 1
+print 0
---
+++ tests/whilecomplex.py
@@ -0,0 +1,8 @@
+x = input()
+while x > 1:
+    if x < 2:
+       print x
+    else:
+       print 0
+    x = x - 1
+
